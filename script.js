c70deaa5efb6bae09ed598aa93fa3d4894b6d04e
Azmanur1 <azmanurrahman557@gmail.com>
2025-02-03 22:20:54 +0600

    Done project

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const screenText = document.querySelector(".screen");
 
-// Todo: Make M+ M- and MC functional
 let string = "";
 let bracketClick = 0;
-// let test = '' //delete later after testing;
+let result = "";
+let len = 0;
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
-  //  console.log(e.target.innerHTML);
-
+    string = String(string);
     if(e.target.innerHTML == '='){
       string = string.replace(/x/g, "*").replace(/÷/g, "/"); // Convert symbols
-      string = eval(string);
-      console.log(string);
+      // for handlig erro that can occur in eval();
+      try {
+        string = eval(string);
+      } catch (error) {
+        string = string;
+        console.log(error);
+      }
       document.querySelector('input').value = string;
     }
     else if(e.target.innerHTML == '←'){
-      console.log(e.target.innerHTML);
-      let len = string.length;
-      let result = string.slice(0,len-1);
-       string = result;
-       document.querySelector('input').value = string;
-       console.log(string);
+       len = string.length;
+       if(len > 0)
+       {
+        result = string.slice(0,len-1);
+        string = result;
+        document.querySelector('input').value = string;
+       }
+       
 
     }
 
@@ -36,12 +42,12 @@ Array.from(buttons).forEach((button)=>{
       bracketClick %= 2;
       document.querySelector('input').value = string;
     }
+
     else if(e.target.innerHTML == 'C'){
       string = ""
       document.querySelector('input').value = string;
     }
     else{ 
-    // console.log(e.target)
     string = string + e.target.innerHTML;
     document.querySelector('input').value = string;
       }
